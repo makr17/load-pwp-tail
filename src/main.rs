b53b4d9f727264d02877d86d8b0a5954e14afae8
makr17 <bracher@gmail.com>
2017-01-11 11:34:15 -0800

    store new samples from STDIN in the database

--- src/main.rs
+++ src/main.rs
@@ -18,24 +18,36 @@ fn main() {
     // connect to the db
     let user  = env::var("USER").unwrap();
     let db    = "pwp";
+    let table = "power";
     let dburi = ["postgres://", &user, "@%2Frun%2Fpostgresql", "/", &db].concat();
     let conn = Connection::connect(dburi, TlsMode::None).unwrap();
 
-    let last = last_sample("power", &conn);
-    println!("{:?}", last);
-    
-//    let separators : &[char] = &[','];
-//    let stdin = io::stdin();
-//    for line in stdin.lock().lines() {
-//        let input = line.unwrap();
-//        let mut tokens: Vec<&str> = input.split(separators).collect();
-//        let sample = Sample {
-//            sampled:  DateTime::parse_from_rfc3339(tokens[0]).unwrap(),
-//            meter_id: tokens[3].parse::<i32>().unwrap(),
-//            value:    tokens[7].parse::<i32>().unwrap()
-//        };
-//        println!("{:?}", sample);
-//    }
+    let last = last_sample(table, &conn);
+
+    let insert = [
+        "insert into ",
+        &table,
+        " (sampled, meter_id, value)",
+        " values ($1, $2, $3)"
+    ].concat();
+    let stmt = conn.prepare(&insert).unwrap();
+    let separators : &[char] = &[','];
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let input = line.unwrap();
+        let tokens: Vec<&str> = input.split(separators).collect();
+        let sample = Sample {
+            sampled:  DateTime::parse_from_rfc3339(tokens[0]).unwrap(),
+            meter_id: tokens[3].parse::<i32>().unwrap(),
+            value:    tokens[7].parse::<i32>().unwrap()
+        };
+        // skip anything before the most recent sample
+        // TODO: sample might have more precision, yielding one error at start
+        if sample.sampled >= last.sampled {
+            println!("{:?}", sample);
+            let _res = stmt.execute(&[&sample.sampled, &sample.meter_id, &sample.value]);
+        }
+    }
 }
 
 fn last_sample (table: &str, conn: &postgres::Connection) -> Sample {
@@ -54,3 +66,4 @@ fn last_sample (table: &str, conn: &postgres::Connection) -> Sample {
     };
     return sample;
 }
+
